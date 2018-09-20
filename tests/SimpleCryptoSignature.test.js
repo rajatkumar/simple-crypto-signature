4471b0c7ccfc6df6f8624b4ebb9d5d56161f78f6
Rajat Kumar <rajatk@netflix.com>
2018-09-20 14:30:28 -0700

    fix: unit tests to support new functions

--- tests/SimpleCryptoSignature.test.js
+++ tests/SimpleCryptoSignature.test.js
@@ -9,12 +9,17 @@ const defaultOptions = {
     passPhrase: 'rajat'
 };
 
+let globalSignedValue = '';
+let globalMessage = '';
+
 describe('SimpleCryptoSignature', () => {
     test('should sign the message', () => {
         const message = 'test-this-string';
         signatureGenerator = new SimpleCryptoSignature(defaultOptions);
         const signedValue = signatureGenerator.sign(message);
         expect(signedValue).toBeTruthy();
+        globalSignedValue = signedValue;
+        globalMessage = message;
     });
 
     test('should produce same signatures', () => {
@@ -55,6 +60,88 @@ describe('SimpleCryptoSignature', () => {
         );
         expect(verifiedValue).toEqual(true);
         expect(verifiedValueTwo).toEqual(true);
+    });
+});
+
+describe('SimpleCryptoSignature with Safe Functions', () => {
+    test('should sign the message', (done) => {
+        const message = 'test-this-string';
+        signatureGenerator = new SimpleCryptoSignature(defaultOptions);
+        signatureGenerator.safeSign(message, function(err, signedValue) {
+            expect(err).toBeNull();
+            expect(signedValue).toBeTruthy();
+            done();
+        });
+    });
+
+    test('should produce same signatures', (done) => {
+        const message = 'test-this-string';
+        signatureGenerator = new SimpleCryptoSignature(defaultOptions);
+        let signedValueOne, signedValueTwo;
+        signatureGenerator.safeSign(message, function(err, value) {
+            expect(err).toBeNull();
+            signedValueOne = value;
+            signatureGenerator.safeSign(message, function(err2, value2) {
+                expect(err2).toBeNull();
+                signedValueTwo = value2;
+                expect(signedValueOne).toEqual(signedValueTwo);
+                done();
+            });
+        });
+    });
+
+    test('should produce different signatures', (done) => {
+        const message = 'test-this-string';
+        const messageTwo = 'test-this-string-2';
+        signatureGenerator = new SimpleCryptoSignature(defaultOptions);
+        let signedValueOne, signedValueTwo;
+        signatureGenerator.safeSign(message, function(err, value) {
+            expect(err).toBeNull();
+            signedValueOne = value;
+            signatureGenerator.safeSign(messageTwo, function(err2, value2) {
+                expect(err2).toBeNull();
+                signedValueTwo = value2;
+                expect(signedValueOne).not.toEqual(signedValueTwo);
+                done();
+            });
+        });
+    });
+
+    test('should verify signature', (done) => {
+        const message = 'test-this-string';
+        signatureGenerator = new SimpleCryptoSignature(defaultOptions);
+        const signedValue = signatureGenerator.sign(message);
+        signatureGenerator.safeVerify(message, signedValue, function(
+            err,
+            verifiedValue
+        ) {
+            expect(err).toBeNull();
+            expect(verifiedValue).toEqual(true);
+            done();
+        });
+    });
+
+    test('should verify different signatures', () => {
+        const message = 'test-this-string';
+        const messageTwo = 'test-this-string-2';
+        signatureGenerator = new SimpleCryptoSignature(defaultOptions);
+        const signedValue = signatureGenerator.sign(message);
+        const signedValueTwo = signatureGenerator.sign(messageTwo);
+
+        signatureGenerator.safeVerify(message, signedValue, function(
+            err,
+            verifiedValue
+        ) {
+            expect(err).toBeNull();
+            expect(verifiedValue).toEqual(true);
+            signatureGenerator.verify(messageTwo, signedValueTwo, function(
+                err2,
+                verifiedValueTwo
+            ) {
+                expect(err2).toBeNull();
+                expect(verifiedValueTwo).toEqual(true);
+            });
+        });
     });
 });
 
@@ -99,6 +186,45 @@ describe('SimpleCryptoSignature Negative Cases', () => {
         } catch (error) {
             expect(error).toBeTruthy();
         }
+    });
+    test('should throw error if no passphrase is provided', (done) => {
+        const message = 'test-this-string';
+        signatureGenerator = new SimpleCryptoSignature({
+            privateKeyPath: path.join(__dirname, 'private.pem'),
+            publicKeyPath: path.join(__dirname, 'public.pem')
+        });
+        signatureGenerator.safeSign(message, function(err, sign) {
+            expect(err).toBeTruthy();
+            expect(sign).toBeUndefined();
+            done();
+        });
+    });
+    test('should throw error if no public key is provided', (done) => {
+        signatureGenerator = new SimpleCryptoSignature({
+            privateKeyPath: path.join(__dirname, 'private.pem'),
+            passPhrase: 'rajat'
+        });
+        signatureGenerator.safeVerify(
+            globalMessage,
+            globalSignedValue,
+            function(err, data) {
+                expect(err).toBeTruthy();
+                expect(data).toBeUndefined();
+                done();
+            }
+        );
+    });
+    test('should throw error if no private key is provided', (done) => {
+        const message = 'test-this-string';
+        signatureGenerator = new SimpleCryptoSignature({
+            publicKeyPath: path.join(__dirname, 'public.pem'),
+            passPhrase: 'rajat'
+        });
+        signatureGenerator.safeSign(message, function(err, sign) {
+            expect(err).toBeTruthy();
+            expect(sign).toBeUndefined();
+            done();
+        });
     });
 });
 
